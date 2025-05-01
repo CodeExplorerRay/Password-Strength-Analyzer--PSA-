181cf46f1eb08e4c1d1b4655458cb098c3e7c8a0
learnaisimplified <learnaisimplified@gmail.com>
2025-05-01 16:16:12 +0800

    Fix: bind Flask to 0.0.0.0 and dynamic port

--- analyze.py
+++ analyze.py
@@ -82,4 +82,5 @@ def serve_static(filename):
     return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
